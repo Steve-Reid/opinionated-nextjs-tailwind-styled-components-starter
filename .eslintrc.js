2542abd10666e721af3f5a704b3b3bd927c26de5
SteveReid <steve@appby.design>
2021-12-19 15:24:20 +0000

    install and set up storybook

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['plugin:storybook/recommended', 'next/core-web-vitals'],
+  overrides: [
+    {
+      // or whatever matches stories specified in .storybook/main.js
+      files: ['*.stories.@(ts|tsx|js|jsx|mjs|cjs)'],
+      rules: {
+        // example of overriding a rule
+        'storybook/hierarchy-separator': 'error',
+        // example of disabling a rule
+        'storybook/default-exports': 'off',
+      },
+    },
+  ],
+};
