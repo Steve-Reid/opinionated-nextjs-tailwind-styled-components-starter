9d7ceca69eb2335a665a8c207a658abcad22b7f7
SteveReid <steve@appby.design>
2022-01-25 18:50:08 +0000

    install and integrate Styled Components with Tailwind via twin.macro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,57 @@
 import type { NextPage } from 'next';
-import styles from '../styles/Home.module.css';
+import tw, { styled } from 'twin.macro';
+
+// still works despite importing from twin.macro
+const StyledButton = styled.button`
+  background: red;
+  color: white;
+  font-size: 1em;
+  text-align: center;
+  padding: 0.25em 1em;
+  border: 2px solid black;
+  margin: 8px 0;
+`;
+
+const TailwindButton = tw.button`
+  bg-red-500
+  hover:bg-red-700
+  text-white
+  font-bold
+  py-2
+  px-4
+  border
+  border-black
+  rounded
+  mx-4
+`;
+
+interface ConditionalButtonProps {
+  isRed?: boolean;
+}
+
+const ConditionalButton = styled.button(({ isRed }: ConditionalButtonProps) => [
+  isRed ? tw`bg-red-500 hover:bg-red-700` : tw`bg-blue-500 hover:bg-blue-500`,
+  tw`
+    text-white
+    font-bold
+    py-2
+    px-4
+    border
+    border-black
+    rounded
+    mx-4
+  `,
+]);
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-3xl font-bold underline mb-2">Hello world!</h1>
+        <TailwindButton>Tailwind Button</TailwindButton>
+        <StyledButton>Styled Button</StyledButton>
+        <ConditionalButton isRed>Conditional Button</ConditionalButton>
+      </main>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  content: [
+  mode: 'jit',
+  purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkmode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
