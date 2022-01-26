e6354d1dad6e237cc8346bcdfee537b2b3d01ad5
Steve Reid <steve@appby.design>
2022-01-26 11:07:37 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/customStyledComponents/**/*.{js,ts,jsx,tsx}',
   ],
   darkmode: false,
   theme: {
